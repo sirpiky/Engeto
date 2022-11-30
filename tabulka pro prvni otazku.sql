272f9ca822b6a6ee41e0d2d77902e6fd4d43848f
Jiri <40075293+sirpiky@users.noreply.github.com>
2022-11-30 17:45:21 +0100

    Add files via upload
    Tabulky pro prvni otazku

---
+++ tabulka pro prvni otazku.sql
@@ -0,0 +1,100 @@
+/*Zemědělství, lesnictví, rybářství*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'A';
+
+/*Těžba a dobývání*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'B';
+
+/*Zpracovatelský průmysl*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'C';
+
+/*Výroba a rozvod elektřiny, plynu, tepla a klimatiz. vzduchu*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'D';
+
+/*Zásobování vodou; činnosti související s odpady a sanacemi*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'E';
+
+/*Stavebnictví*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'F';
+
+/*Velkoobchod a maloobchod; opravy a údržba motorových vozidel*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'G';
+
+/*Doprava a skladování*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'H';
+
+/*Ubytování, stravování a pohostinství*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'I';
+
+/*Informační a komunikační činnosti*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'J';
+
+/*Peněžnictví a pojišťovnictví*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'K';
+
+/*Činnosti v oblasti nemovitostí*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'L';
+
+/*Profesní, vědecké a technické činnosti*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'M';
+
+/*Administrativní a podpůrné činnosti*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'N';
+
+/*Veřejná správa a obrana; povinné sociální zabezpečení*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'O';
+
+/*Vzdělávání*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'P';
+
+/*Zdravotní a sociální péče*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'Q';
+
+/*Kulturní, zábavní a rekreační činnosti*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'R';
+
+/*Ostatní činnosti*/
+SELECT *
+FROM czechia_payroll cp 
+WHERE industry_branch_code = 'S';
+
+
+
+
+
+
